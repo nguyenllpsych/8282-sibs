72bafa006151f12fec50a35746057cc80dc8b9f1
Linh Nguyen <nguy4006@umn.edu>
2022-03-04 14:58:59 -0600

    clean relationship quality data

--- 00_REL-cleaning.R
+++ 00_REL-cleaning.R
@@ -30,8 +30,35 @@ rel_FU1 <- rel_FU1 %>%
 
 # scale scores
 for (dim in unique(na.omit(dict$scale))) {
+  
+  # pull item names for each sub scale
   items <- dict %>% filter(scale == dim) %>%
     pull(variable)
-  assign(x = dim, value = items)
+  assign(x = "scale", value = items)
   
+  # add column to rel_IN data frame and rename using scale_short from dict
+  rel_IN <- cbind(rel_IN,
+                  rowMeans(select(rel_IN, all_of(scale)),
+                           na.rm = T))
+  names(rel_IN)[ncol(rel_IN)] <- dict[which(dict$scale == dim), 
+                                      "scale_short"][1]
+  
+  # add column to rel_FU1 data frame and rename using scale_short from dict
+  rel_FU1 <- cbind(rel_FU1,
+                   rowMeans(select(rel_FU1, all_of(scale)),
+                            na.rm = T))
+  names(rel_FU1)[ncol(rel_FU1)] <- dict[which(dict$scale == dim), 
+                                        "scale_short"][1]
 }
+
+# check to see who has data at FU1 but not IN
+rel_FU1[which(!rel_FU1$ID %in% rel_IN$ID), "ID"]
+
+# III. Export ----
+
+# only keep ID, IDFAMYR, and REL scale scores
+rel_IN  <- rel_IN  %>% select(-c(Q1:Q48))
+rel_FU1 <- rel_FU1 %>% select(-c(Q1:Q48))
+
+rio::export(rel_IN, file = "./Data/rel_IN.sav")
+rio::export(rel_FU1, file = "./Data/rel_FU1.sav")
