7a9eb209b2d576a889bb96af0a1c34feda00547d
Linh Nguyen <nguy4006@umn.edu>
2022-06-28 15:35:37 +0700

    merge mf items and add var labels to life events

--- 00_LEI-cleaning.R
+++ 00_LEI-cleaning.R
@@ -9,6 +9,7 @@
 
 # load libraries
 libraries <- c("rio",          # import export
+               "labelled",     # var_label
                "reshape2",     # reshape data wide long
                "dplyr",        # general wrangling
                "plyr")         # rbind.fill
@@ -196,8 +197,9 @@ NINF_FU2 <- gsub(pattern = "YN3",
                 x = NINF_FU2)
 
 # rename Q46A -> Q46P
-lei_FU2 <- rename(lei_FU2, Q46P_ = Q46A_)
-lei16_FU1 <- rename(lei16_FU1, Q46P_ = Q46A_)
+lei_FU2 <- dplyr::rename(lei_FU2, Q46P_ = Q46A_)
+lei16_IN <- dplyr::rename(lei16_IN, Q46P_ = Q46A_)
+lei16_FU1 <- dplyr::rename(lei16_FU1, Q46P_ = Q46A_)
 
 FAM_16_IN <- gsub(pattern = "Q46A_", replacement = "Q46P_",
                   x = FAM_16_IN)
@@ -312,5 +314,68 @@ lei$NINF <- lei %>%
   select(all_of(NINF)) %>%
   rowSums(na.rm = T)
 
+# combine M/F items
+lei <- lei %>%
+  mutate(Q29E = as.numeric(Q29EM == "1" | Q29EF == "1"),
+         Q29B = as.numeric(Q29BM == "1" | Q29BF == "1"),
+         Q29C = as.numeric(Q29CM == "1" | Q29CF == "1"),
+         Q29D = as.numeric(Q29DM == "1" | Q29DF == "1"))
+lei <- lei %>%
+  select(-c(Q29EM, Q29EF, Q29BM, Q29BF, Q29CM, Q29CF, Q29DM, Q29DF)) %>%
+  select(ID, IDYRFAM, Q1B:Q22P, Q29E:Q29D, FAM, INF, NINF)
+
 # III. EXPORT ----
+var_label(lei) <- list(
+  Q1B     = "Family moved to new neighborhood",
+  Q3A     = "Changed school due to moving",
+  Q10     = "Family pet died",
+  Q11A    = "Close relative died",
+  Q17     = "Family had money problem",
+  Q17A    = "Family received govt assistance",
+  Q18B    = "Parents argued a lot",
+  Q19AP   = "Parents lived apart",
+  Q20AP   = "Parents dated others",
+  Q21A    = "New adult lived with family",
+  Q21B    = "Moved to other guardian or foster",
+  Q15     = "Siblings ran away",
+  Q22S    = "Siblings unavailable",
+  Q35     = "Family had drug/alcohol trouble",
+  Q41D    = "Family sent to jail",
+  Q49A    = "Family attempted suicide",
+  Q49B    = "Family committed suicide",
+  Q41C    = "Family got arrested",
+  Q7      = "Close friend moved away",
+  Q8      = "Close friend seriously ill/hurt",
+  Q9      = "Close friend died",
+  Q24     = "Body changed due to puberty",
+  Q25     = "Was teased due to body changing",
+  Q26     = "Wore braces",
+  Q27     = "Got pimples",
+  Q29E    = "Partner/self had miscarriage",
+  Q51B    = "Got mugged or robbed",
+  Q4A     = "Got suspended or expelled",
+  Q5      = "Failed to make after-school activity",
+  Q6A     = "Had serious problem with close friend",
+  Q14B    = "Ran away from home",
+  Q20B    = "Started dating",
+  Q28B1   = "Broke up with romantic partner",
+  Q29B    = "Got (a girl) pregnant",
+  Q29C    = "(Partner) gave birth",
+  Q29D    = "(Partner) got an abortion",
+  Q38B    = "Had trouble with police",
+  Q38H    = "Went to court",
+  Q38C    = "Sent to juvenile detention",
+  Q43F    = "Family got treated for mental problem",
+  Q46P    = "Family got hospitalized for mental problem",
+  Q13     = "Moved away from home",
+  Q30A    = "Moved in with romantic partner",
+  Q31     = "Got married",
+  Q16F    = "Failed to get a job",
+  Q16D    = "Started a job",
+  Q16B    = "Lost a job",
+  Q22P    = "Parents unavailable",
+  FAM     = "Count of family-related events",
+  INF     = "Count of independent non-family events",
+  NINF    = "Count of non-independent non-family events"
+)
 rio::export(x = lei, file = "./Data/lei.sav")
